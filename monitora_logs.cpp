04626b70dc6d01c7acf10cf571f4568826c65f61
Joao Victor <joao.araruna8@gmail.com>
2026-05-24 22:14:44 -0300

    feat(green): TD01 implementa deteccao de lista inexistente

--- monitora_logs.cpp
+++ monitora_logs.cpp
@@ -1,9 +1,15 @@
 #include "monitora_logs.hpp"
 
+#include <fstream>
+
 namespace monitora_logs {
 
 ResultadoMonitoramento MonitorarLogs(const std::string& caminho_lista_logs) {
-  (void)caminho_lista_logs;
+  std::ifstream lista_logs(caminho_lista_logs);
+  if (!lista_logs.is_open()) {
+    return {CodigoResultado::kListaLogsInexistente};
+  }
+
   return {CodigoResultado::kSucesso};
 }
 
