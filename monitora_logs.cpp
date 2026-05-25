8d5dee5e7489299ad845bd268282565674534d33
Joao Victor <joao.araruna8@gmail.com>
2026-05-24 22:15:20 -0300

    refactor: TD01 organiza resultado de monitoramento

--- monitora_logs.cpp
+++ monitora_logs.cpp
@@ -4,13 +4,21 @@
 
 namespace monitora_logs {
 
+namespace {
+
+ResultadoMonitoramento CriarResultado(CodigoResultado codigo) {
+  return {codigo};
+}
+
+}  // namespace
+
 ResultadoMonitoramento MonitorarLogs(const std::string& caminho_lista_logs) {
   std::ifstream lista_logs(caminho_lista_logs);
   if (!lista_logs.is_open()) {
-    return {CodigoResultado::kListaLogsInexistente};
+    return CriarResultado(CodigoResultado::kListaLogsInexistente);
   }
 
-  return {CodigoResultado::kSucesso};
+  return CriarResultado(CodigoResultado::kSucesso);
 }
 
 }  // namespace monitora_logs
