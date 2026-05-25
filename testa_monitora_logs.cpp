303ac44b88d5876226a67f39cd0419ecdad26caa
Joao Victor <joao.araruna8@gmail.com>
2026-05-24 22:23:48 -0300

    test(red): TD05 cria total para log valido sem total anterior

--- testa_monitora_logs.cpp
+++ testa_monitora_logs.cpp
@@ -69,3 +69,39 @@ TEST_CASE("TD04 log listado inexistente e ignorado", "[td04]") {
   REQUIRE(resultado.logs_ignorados == 1);
   REQUIRE_FALSE(std::filesystem::exists(diretorio_teste / "total_log1.txt"));
 }
+
+TEST_CASE("TD05 cria total para log valido sem total anterior", "[td05]") {
+  const std::filesystem::path diretorio_teste =
+      std::filesystem::temp_directory_path() / "monitora_logs_td05";
+  std::filesystem::remove_all(diretorio_teste);
+  std::filesystem::create_directories(diretorio_teste);
+
+  const std::filesystem::path caminho_log = diretorio_teste / "log1.txt";
+  std::ofstream log(caminho_log);
+  log << "20/1/2026 17:45:38  Segundo registro\n";
+  log << "16/1/2026 13:27:46  Primeiro registro\n";
+  log.close();
+
+  const std::filesystem::path caminho_lista = diretorio_teste / "logs.txt";
+  std::ofstream lista(caminho_lista);
+  lista << caminho_log.string() << "\n";
+  lista.close();
+
+  const monitora_logs::ResultadoMonitoramento resultado =
+      monitora_logs::MonitorarLogs(caminho_lista.string());
+
+  const std::filesystem::path caminho_total =
+      diretorio_teste / "total_log1.txt";
+  REQUIRE(resultado.codigo == monitora_logs::CodigoResultado::kSucesso);
+  REQUIRE(resultado.logs_processados == 1);
+  REQUIRE(std::filesystem::exists(caminho_total));
+
+  std::ifstream total(caminho_total);
+  std::string primeira_linha;
+  std::string segunda_linha;
+  std::getline(total, primeira_linha);
+  std::getline(total, segunda_linha);
+
+  REQUIRE(primeira_linha == "16/1/2026 13:27:46  Primeiro registro");
+  REQUIRE(segunda_linha == "20/1/2026 17:45:38  Segundo registro");
+}
